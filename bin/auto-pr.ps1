3a8f4f33e8aa8930a60414c92d8ccd83b2413ac3
h404bi <h404bi@gmail.com>
2017-09-02 16:23:28 +0800

    scoop bucket and app manifest standard

---
+++ bin/auto-pr.ps1
@@ -0,0 +1,9 @@
+param(
+    # overwrite upstream param
+    [String]$upstream = "h404bi/dorado:master"
+)
+
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
+$autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
+$dir = "$psscriptroot/.." # checks the parent dir
+iex -command "$autopr -dir $dir -upstream $upstream $($args |% { "$_ " })"
