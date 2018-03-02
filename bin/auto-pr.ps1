8083ecc0d720dfc522a0de7f4ce21a694ea69968
h404bi <h404bi@gmail.com>
2018-03-02 22:37:12 +0800

    Tweaks

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -6,4 +6,4 @@ param(
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
 $dir = "$psscriptroot/.." # checks the parent dir
-iex -command "$autopr -dir $dir -upstream $upstream $($args |% { "$_ " })"
+Invoke-Expression -command "$autopr -dir $dir -upstream $upstream $($args | ForEach-Object { "$_ " })"
--- bin/formatjson.ps1
+++ bin/formatjson.ps1
@@ -1,4 +1,4 @@
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
 $formatjson = "$env:SCOOP_HOME/bin/formatjson.ps1"
 $dir = "$psscriptroot/.." # checks the parent dir
-iex -command "$formatjson -dir $dir $($args |% { "$_ " })"
+Invoke-Expression -command "$formatjson -dir $dir $($args | ForEach-Object { "$_ " })"
