3d47336774ce58607a914e2ed2990344fa01827e
h404bi <h404bi@gmail.com>
2019-01-28 14:21:55 +0800

    Fix bins [ci skip]

--- bin/formatjson.ps1
+++ bin/formatjson.ps1
@@ -1,4 +1,4 @@
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
 $formatjson = "$env:SCOOP_HOME/bin/formatjson.ps1"
-$dir = "$psscriptroot/.." # checks the parent dir
-Invoke-Expression -command "$formatjson -dir $dir $($args | ForEach-Object { "$_ " })"
+$path = "$psscriptroot/../bucket" # checks the parent dir
+Invoke-Expression -command "$formatjson -path $path $($args | ForEach-Object { "$_ " })"
