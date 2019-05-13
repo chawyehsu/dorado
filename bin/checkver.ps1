d0d32369d1a61d9dfe5de48e4430fe8e49c2c0e7
h404bi <h404bi@outlook.com>
2019-05-14 00:09:19 +0800

    chore: update checkver [ci skip]

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -1,4 +1,14 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-$checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
-$dir = "$psscriptroot/../bucket" # checks the parent dir
-Invoke-Expression -command "$checkver -dir $dir $($args | ForEach-Object { "$_ " })"
+param(
+    [String] $dir = "$PSScriptRoot\..\bucket",
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [String[]] $remainArgs = @()
+)
+
+if (!$env:SCOOP_HOME) {
+    $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop)))
+}
+
+$checkver = "$env:SCOOP_HOME\bin\checkver.ps1"
+$remainArgs = ($remainArgs | Select-Object -Unique) -join ' '
+
+Invoke-Expression -command "$checkver -dir $dir $remainArgs"
