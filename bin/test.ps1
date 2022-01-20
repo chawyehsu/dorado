2b92aba53a0f8849c36f3d1ef2f12cecc7c514b7
Chawye Hsu <chawyehsu@hotmail.com>
2022-01-20 16:11:50 +0800

    ci: Fix typo
    Signed-off-by: Chawye Hsu <chawyehsu@hotmail.com>

--- bin/test.ps1
+++ bin/test.ps1
@@ -2,4 +2,4 @@
 #Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '4.4.0' }
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $result = Invoke-Pester "$PSScriptRoot\..\test" -PassThru
-exit $result.FaildCount
+exit $result.FailedCount
