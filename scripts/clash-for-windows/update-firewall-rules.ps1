b36f8138ef003b07f6c767bcfad8fe63bad56362
AkariiinMKII <xbloou@gmail.com>
2022-09-04 18:23:07 +0800

    Update update-firewall-rules.ps1

--- scripts/clash-for-windows/update-firewall-rules.ps1
+++ scripts/clash-for-windows/update-firewall-rules.ps1
@@ -1,9 +1,9 @@
-Remove-NetFirewallRule -Description "Work with Clash for Windows v* installed by scoop/dorado." -ErrorAction SilentlyContinue
+Remove-NetFirewallRule -Description "Work with Clash for Windows." -ErrorAction SilentlyContinue
 'TCP', 'UDP' | ForEach-Object {
     New-NetFirewallRule `
         -DisplayName "Clash for Windows" `
         -Profile "Private, Public" `
-        -Description "Work with Clash for Windows v$version installed by scoop/dorado." `
+        -Description "Work with Clash for Windows." `
         -Direction Inbound `
         -Protocol $_ `
         -Action Allow `
@@ -13,7 +13,7 @@ Remove-NetFirewallRule -Description "Work with Clash for Windows v* installed by
     New-NetFirewallRule `
         -DisplayName "Clash Core" `
         -Profile "Private, Public" `
-        -Description "Work with Clash for Windows v$version installed by scoop/dorado." `
+        -Description "Work with Clash for Windows." `
         -Direction Inbound `
         -Protocol $_ `
         -Action Allow `
