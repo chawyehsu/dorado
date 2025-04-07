77eb863723e5ea9885a14aeaad7e9216c980cf24
Chawye Hsu <su+git@chawyehsu.com>
2025-04-07 23:04:55 +0800

    chore: fix test
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- test/Scoop-Bucket.Tests.ps1
+++ test/Scoop-Bucket.Tests.ps1
@@ -1,4 +1,4 @@
 if (!$env:SCOOP_HOME) {
-    $env:SCOOP_HOME = Resolve-Path (Split-Path (Split-Path (scoop which scoop))) 
+    $env:SCOOP_HOME = Resolve-Path (Split-Path (Split-Path (scoop which scoop)))
 }
 . "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1"
